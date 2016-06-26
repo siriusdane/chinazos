89160c21296b52fd68ac962be638c8a03966dc6e
Aquiles Lacruz <aquiles.lacruz@gmail.com>
2016-06-26 18:11:14 -0430

    Settings: Still tinkering with the statics

--- Chinazos/settings.py
+++ Chinazos/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'WebPanel', 'static'),
 )
 
 # Simplified static file serving.
