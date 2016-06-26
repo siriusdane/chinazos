58db4eba21f34c6546c6b5cfe2c7f6d783427bd7
Aquiles Lacruz <aquiles.lacruz@gmail.com>
2016-06-26 19:19:04 -0430

    Added register form as dialog

--- WebPanel/static/WebPanel/js/Base/inner.js
+++ WebPanel/static/WebPanel/js/Base/inner.js
@@ -29,6 +29,13 @@ dialog_login.querySelector('.close').addEventListener('click', function() {
     dialog_login.close();
 });
 
+// Register Dialog
+var dialog_register = document.querySelector('#dialog_register');
+if (! dialog_register.showModal) { dialogPolyfill.registerDialog(dialog_register); }
+dialog_register.querySelector('.close').addEventListener('click', function() {
+    dialog_register.close();
+});
+
 function login() {
     $.ajax({
         url: url_loginService,
@@ -49,14 +56,35 @@ function logout() {
     })
 }
 
+function register() {
+    $.ajax({
+        url: url_registerService,
+        method: 'POST',
+        data: $('#form_register').serialize(),
+        dataType: 'text',
+        success: function () { location.reload() },
+        error: function () { show_register_error() }
+    })
+}
+
 function show_login() {
     $('#form_login input').val('');
     $('#span_loginError').css('visibility', 'hidden');
     dialog_login.showModal();
 }
 
+function show_register() {
+    $('#form_register input').val('');
+    $('#span_registerError').css('visibility', 'hidden');
+    dialog_register.showModal();
+}
+
 function show_login_error() {
     $('#span_loginError').css('visibility', 'visible');
+}
+
+function show_register_error() {
+    $('#span_registerError').css('visibility', 'visible');
 }
 
 function gotoHome() {
