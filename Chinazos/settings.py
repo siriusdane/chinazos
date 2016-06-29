7ca61212dc801c7d795cdce64bdb262c622f2826
Aquiles Lacruz <aquiles.lacruz@gmail.com>
2016-06-29 00:45:04 -0430

    Settings: Openshift settings

--- Chinazos/settings.py
+++ Chinazos/settings.py
@@ -11,23 +11,37 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+DJ_PROJECT_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(DJ_PROJECT_DIR)
+WSGI_DIR = os.path.dirname(BASE_DIR)
+REPO_DIR = os.path.dirname(WSGI_DIR)
+DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-WEBPANEL_ROOT = os.path.join(BASE_DIR, 'WebPanel')
-
+import sys
+sys.path.append(os.path.join(REPO_DIR, 'libs'))
+import secrets
+SECRETS = secrets.getter(os.path.join(DATA_DIR, 'secrets.json'))
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gyb&#w@%fzoqrac5-5+)^+pod%nj!=pzy@prv+kgr=k!_!%-!v'
+SECRET_KEY = SECRETS['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+from socket import gethostname
+ALLOWED_HOSTS = [
+    gethostname(), # For internal OpenShift load balancer security purposes.
+    os.environ.get('OPENSHIFT_APP_DNS'), # Dynamically map to the OpenShift gear name.
+    #'example.com', # First DNS alias (set up in the app)
+    #'www.example.com', # Second DNS alias (set up in the app)
+]
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+WEBPANEL_ROOT = os.path.join(BASE_DIR, 'WebPanel')
 
 
 # Application definition
@@ -82,8 +96,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'chinazos',
-        'USER': 'adminhuthwgm',
-        'PASSWORD': 'XYznf9KEaiib',
+        'USER': 'admin46if5dm',
+        'PASSWORD': 'q3KN1fk_jGPL',
         'HOST': os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
         'PORT': os.environ['OPENSHIFT_POSTGRESQL_DB_PORT']
     }
@@ -113,8 +127,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
